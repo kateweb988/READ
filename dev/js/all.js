8a6620e69d859bdf52b28750149b3d88814b2fab
kateweb988 <83764959+kateweb988@users.noreply.github.com>
2025-02-27 18:05:00 +0200

    Update all.js

--- dev/js/all.js
+++ dev/js/all.js
@@ -629,6 +629,204 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  $(document).ready(function () {
+    $('[data-submit2]').on('click', function (e) {
+      e.preventDefault();
+      $(this).parents('form').submit();
+    })
+    $.validator.addMethod(
+      "regex",
+      function (value, element, regexp) {
+        var re = new RegExp(regexp);
+        return this.optional(element) || re.test(value);
+      },
+      "Please check your input."
+    );
+    function valEl(el) {
+
+      el.validate({
+        rules: {
+          tel: {
+            required: true,
+            regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+          },
+          name: {
+            required: true
+          },
+          pass: {
+            required: true
+          },
+          login: {
+            required: true
+          },
+          text: {
+            required: true
+          },
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          tel: {
+            required: 'Поле обязательно для заполнения',
+            regex: 'Телефон может содержать символы + - ()'
+          },
+          name: {
+            required: 'Поле обязательно для заполнения',
+          },
+          login: {
+            required: 'Поле обязательно для заполнения',
+          },
+          pass: {
+            required: 'Поле обязательно для заполнения',
+          },
+          email: {
+            required: 'Поле обязательно для заполнения',
+            email: 'Неверный формат E-mail'
+          }
+        },
+        submitHandler: function (form) {
+          $('#loader').fadeIn();
+          var $form = $(form);
+          var $formId = $(form).attr('id');
+          switch ($formId) {
+            case 'popupResult2':
+              $.ajax({
+                type: 'POST',
+                url: $form.attr('action'),
+                data: $form.serialize(),
+              })
+                .always(function (response) {
+                  setTimeout(function () {
+                    $('#loader').fadeOut();
+                  }, 800);
+                  setTimeout(function () {
+                    $.arcticmodal('close');
+                    $('#popup-thank').arcticmodal({});
+                    $form.trigger('reset');
+                    //строки для остлеживания целей в Я.Метрике и Google Analytics
+                  }, 1100);
+
+                });
+              break;
+          }
+          return false;
+        }
+      })
+    }
+
+    $('.js-form2').each(function () {
+      valEl($(this));
+    });
+    $('[data-scroll]').on('click', function () {
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'data-scroll')).offset().top
+      }, 2000);
+      event.preventDefault();
+    })
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  $(document).ready(function () {
+    $('[data-submit3]').on('click', function (e) {
+      e.preventDefault();
+      $(this).parents('form').submit();
+    })
+    $.validator.addMethod(
+      "regex",
+      function (value, element, regexp) {
+        var re = new RegExp(regexp);
+        return this.optional(element) || re.test(value);
+      },
+      "Please check your input."
+    );
+    function valEl(el) {
+
+      el.validate({
+        rules: {
+          tel: {
+            required: true,
+            regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+          },
+          name: {
+            required: true
+          },
+          pass: {
+            required: true
+          },
+          login: {
+            required: true
+          },
+          text: {
+            required: true
+          },
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          tel: {
+            required: 'Поле обязательно для заполнения',
+            regex: 'Телефон может содержать символы + - ()'
+          },
+          name: {
+            required: 'Поле обязательно для заполнения',
+          },
+          login: {
+            required: 'Поле обязательно для заполнения',
+          },
+          pass: {
+            required: 'Поле обязательно для заполнения',
+          },
+          email: {
+            required: 'Поле обязательно для заполнения',
+            email: 'Неверный формат E-mail'
+          }
+        },
+        submitHandler: function (form) {
+          $('#loader').fadeIn();
+          var $form = $(form);
+          var $formId = $(form).attr('id');
+          switch ($formId) {
+            case 'popupResult3':
+              $.ajax({
+                type: 'POST',
+                url: $form.attr('action'),
+                data: $form.serialize(),
+              })
+                .always(function (response) {
+                  setTimeout(function () {
+                    $('#loader').fadeOut();
+                  }, 800);
+                  setTimeout(function () {
+                    $.arcticmodal('close');
+                    $('#popup-thank').arcticmodal({});
+                    $form.trigger('reset');
+                    //строки для остлеживания целей в Я.Метрике и Google Analytics
+                  }, 1100);
+
+                });
+              break;
+          }
+          return false;
+        }
+      })
+    }
+
+    $('.js-form3').each(function () {
+      valEl($(this));
+    });
+    $('[data-scroll]').on('click', function () {
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'data-scroll')).offset().top
+      }, 2000);
+      event.preventDefault();
+    })
+  });
+});
 document.addEventListener("DOMContentLoaded", () => {
   let menuBtn = document.querySelector('.menu-btn');
   let menu = document.querySelector('.menu');
